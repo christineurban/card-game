9650491ac431a0351c7147f7e89979405dc77b81
Christine Urban <christineurbansf@gmail.com>
2018-09-27 22:47:50 -0700

    Implement basic deck functionality

---
+++ src/lib/card.js
@@ -0,0 +1,10 @@
+class Card {
+    constructor(suit, value, name) {
+        this._suit = suit; // Suit
+        this._value = value; // int
+        this._name = name; // string
+    }
+}
+
+
+module.exports = Card;
---
+++ src/lib/suit.js
@@ -0,0 +1,9 @@
+class Suit {
+    constructor(name, color) {
+        this._name = name; // string
+        this._color = color; // string
+    }
+}
+
+
+module.exports = Suit;
