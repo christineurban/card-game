acd4747ae62e90fd0a749de663d73ea3b2ae1f55
Christine Urban <christineurbansf@gmail.com>
2018-10-19 12:28:33 -0700

    Fix tests

---
+++ test/src/lib/deck-builder.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const assert = require('assert');
+const expect = require('chai').expect;
+
+const build = require('../../../src/lib/deck-builder');
+const Card = require('../../../src/lib/card');
+const Deck = require('../../../src/lib/deck');
+
+
+describe('Deck Builder', function() {
+    context('build()', function() {
+        it('should return an array of Cards', function() {
+            const deck = new Deck(build());
+
+            expect(deck._cards).to.be.an('array');
+            expect(deck._cards.length).to.deep.equal(52);
+            expect(deck._cards[0]).to.be.instanceOf(Card);
+        });
+    });
+});
